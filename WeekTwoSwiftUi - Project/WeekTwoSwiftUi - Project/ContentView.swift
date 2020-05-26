d803920cdc8a9fc9600608609d712caab05620b3
whatcodehaveyougit <siggywatt@yahoo.com>
2020-05-26 10:02:40 +0100

    buttons working and css fine

--- WeekTwoSwiftUi - Project/WeekTwoSwiftUi - Project/ContentView.swift
+++ WeekTwoSwiftUi - Project/WeekTwoSwiftUi - Project/ContentView.swift
@@ -10,53 +10,54 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var movies = ["film 1", "film 2"]
-    
-    var body: some View {
-        NavigationView{
-            
-            ZStack {
-                       Image("batman")
-                           .resizable()
-                           .edgesIgnoringSafeArea(.all)
+var movies = ["film 1", "film 2"]
 
-                       VStack(alignment: .center) {
-                           HStack {
-                               Text("Batman Movies")
-                               .font(.largeTitle)
-                                   .padding(.all, 30.0)
-                               .background(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
-                           }
-                           Spacer()
-                           VStack {
-                               Text("Dark Night Trilogy")
-                                   .font(.largeTitle)
-                                   .padding(.bottom, 10.0)
-
-                               List {
-                                  Section(header: Text("Dark Night")) {
-                                   ForEach(0..<movies.count) { number in
-                                       NavigationLink(destination:
-                                       MovieView()){
-                                          Text(self.movies[number])
-                                       }
-                                   }
-                                  }
-                               }
-                           }
-                           .padding(.all, 10.0)
-                           .background(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
-                           Spacer()
-
-                       }
-                       
-                   }
-            
-        }
-       
-    }
+var body: some View {
+    NavigationView{
         
+        ZStack {
+           Image("batman")
+               .resizable()
+               .edgesIgnoringSafeArea(.all)
+
+           VStack(alignment: .center) {
+               HStack {
+                   Text("Batman Movies")
+                   .font(.largeTitle)
+                       .padding(.all, 30.0)
+                   .background(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
+               }
+               Spacer()
+               VStack {
+                   Text("Dark Night Trilogy")
+                       .font(.largeTitle)
+                       .padding(.bottom, 10.0)
+
+                       ForEach(0..<movies.count) { number in
+                        VStack {
+                            NavigationLink(destination:
+                                MovieView()){
+                                   Text(self.movies[number])
+                                }
+                                 .foregroundColor(Color.black)
+                                 .padding()
+                                 .border(Color.black, width: 1)
+                                    
+                        }
+                        .padding(.bottom, 10.0)     
+                   }
+               }
+               .padding(.all, 10.0)
+               .background(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/)
+               Spacer()
+        }
     }
+
+}
+
+}
+
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
